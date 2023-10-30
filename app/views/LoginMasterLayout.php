5b4fa8e9d1945020d8015f02297994cd86bc7e67
ithuyln <huylnpc05258@fpt.edu.vn>
2023-10-30 19:51:49 +0700

    update giao diện login/register

--- app/views/LoginMasterLayout.php
+++ app/views/LoginMasterLayout.php
@@ -1,1 +1,4 @@
-<?php require_once "./app/views/pages/" . $data['pages'] . ".php";
+<?php $this->view('blocks/header', $data); ?>
+<?php require_once "./app/views/pages/" . $data['pages'] . ".php"; ?>
+<?php $this->view('blocks/footer', $data); ?>
+
